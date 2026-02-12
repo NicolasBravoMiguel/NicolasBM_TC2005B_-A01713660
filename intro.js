7c89e86e55063468672be5c5b73f8c7f419dd341
NicolasBravoMiguel <a01713660@tec.mx>
2026-02-12 12:49:27 -0600

    Variables, Constantes y sus alcances

---
+++ intro.js
@@ -0,0 +1,21 @@
+// consola (log, info, warn, error, assert)
+console.log("Hola mundo");
+console.info("Creado en el 2009");
+console.warn("Es adictivo");
+console.error("Los tanques no deben de ir atras");
+
+// operador de comparrion de tipo y valor
+console.assert(1 === true);
+
+// operador de comparrion de valores
+console.assert(1== true);
+
+//------------------- Variables, constantes ----------------------
+
+// Forma antigua de declarar variables. Tienen mayor alcanzce por lo que no se recomienda su uso
+var perrsonaje1 = "Gwen";
+
+// Forma nueva de declarar variables. La variable solo vice dentro del ambito donde se declara
+let personaje2 = "Mordekaiser";
+
+const precioSkin = 300;
