e6b87ac4f6f8b5c2fcaf7e9769f88281a444bb8f
NicolasBravoMiguel <a01713660@tec.mx>
2026-02-13 09:43:22 -0600

    prueba

---
+++ lab04.js
@@ -0,0 +1,6 @@
+// -------------------- Caso 1 --------------------
+
+const numero = promt ("Ingresa el numero que deseas evaluar");
+for (numero = 1; i <= n; i++) {
+    document.write(i + " — Cuadrado: " + (i*i) + " — Cubo: " + (i*i*i) + "<br>");
+}
