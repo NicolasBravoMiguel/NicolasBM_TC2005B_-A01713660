765adb22a5b96c0944774d80c448c7f91e34ccb0
NicolasBravoMiguel <a01713660@tec.mx>
2026-02-19 12:40:16 -0600

    creacion de servidor

--- LOL/app.js
+++ LOL/app.js
@@ -1,9 +1,12 @@
 console.log("Hola desde node");
 
-const file = require("fs");
+const filesystem = require("fs");
 
 filesystem.writeFileSync('hola.txt', 'Hola desde node');
 
+setTimeout(() => {
+    console.log("JOJOJO te hackie!");
+}, 15000);
 
 const arreglo = [5000, 60 ,90, 100, 10, 20, 10000, 0, 120, 2000, 340, 1000, 50];
 
