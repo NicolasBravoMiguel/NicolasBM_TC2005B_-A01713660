0e9809fe6171e37ba28afb45f639fe7f52efd434
NicolasBravoMiguel <a01713660@tec.mx>
2026-02-18 23:10:51 -0600

    Correciones de pagina web adaptable a los ejercios del profe

---
+++ LOL/lol.js
@@ -0,0 +1,19 @@
+// document es el pretotipo que contiene el DOM (Document Object model)
+//console.log(document);
+
+const gwen = {
+    nombre : "Gwen",
+    descripcion : `Gwen, una antigua muñeca que se transformó y cobró vida a través de la magia, 
+    usa las mismas herramientas que en su momento la crearon. Lleva el peso del amor
+     de su creadora a cada paso, sin dar nada por sentado. Bajo su mando está la Niebla
+     Sagrada, una magia antigua y protectora que bendijo las tijeras, las agujas y el
+      hilo de coser de Gwen. Muchas cosas son nuevas para Gwen, pero sigue decidida a
+      luchar con gozo por el bien que prevalece en un mundo roto.`,
+    tipo : "mago",
+    imagen: "https://www.google.com/imgres?q=gwen&imgurl=https%3A%2F%2Fddragon.leagueoflegends.com%2Fcdn%2Fimg%2Fchampion%2Fsplash%2FGwen_0.jpg&imgrefurl=https%3A%2F%2Fwww.leagueoflegends.com%2Fes-mx%2Fchampions%2Fgwen%2F&docid=0Zrpr6fPIAy-9M&tbnid=do-v0xLRvXp39M&vet=12ahUKEwjBl-zOzN6SAxVRPUQIHerSLSUQnPAOegQIGxAB..i&w=1215&h=717&hcb=2&ved=2ahUKEwjBl-zOzN6SAxVRPUQIHerSLSUQnPAOegQIGxAB",
+}
+
+const gwen = document.getElementById("gwen");
+console.div(gwen);
+
+
