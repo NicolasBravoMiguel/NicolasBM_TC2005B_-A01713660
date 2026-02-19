446d1cfd44c8dfc8cc632c41e0757577c5043f92
NicolasBravoMiguel <a01713660@tec.mx>
2026-02-19 12:28:41 -0600

    funcion

---
+++ LOL/app.js
@@ -0,0 +1,14 @@
+console.log("Hola desde node");
+
+const file = require("fs");
+
+filesystem.writeFileSync('hola.txt', 'Hola desde node');
+
+
+const arreglo = [5000, 60 ,90, 100, 10, 20, 10000, 0, 120, 2000, 340, 1000, 50];
+
+for (let item of arreglo) {
+    setTimeout(() => {
+        console.log(item);
+    }, item);   
+}
