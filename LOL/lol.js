c9f2dfae95099fddba9454c8363555d485f7bf7d
NicolasBravoMiguel <a01713660@tec.mx>
2026-02-19 11:54:19 -0600

    version de avance a los avances del profe

--- LOL/lol.js
+++ LOL/lol.js
@@ -40,4 +40,96 @@ const div_gwen = document.getElementById("gwen");
 console.log(div_gwen);
 
 
+const mostrar_datos = () => {
+    div_gwen.innerHTML = `
+        <p class="is-size-2">${gwen.nombre}</p>
+        <p>${gwen.descripcion}</p>
+        <span class="tag">${gwen.tipo}</span>
+    `;
+    div_gwen.onclick = mostrar_imagen;
+}
+
+const mostrar_imagen = () => {
+    div_gwen.innerHTML = `
+        <figure class="image">
+            <img class="is-rounded" src="${gwen.imagen}" />
+        </figure>
+    `;
+    div_gwen.onclick = mostrar_datos;
+}
+
+mostrar_imagen();
+
+div_gwen.onclick = mostrar_datos;
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Functions to open and close a modal
+  function openModal($el) {
+    $el.classList.add('is-active');
+  }
+
+  function closeModal($el) {
+    $el.classList.remove('is-active');
+  }
+
+  function closeAllModals() {
+    (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+      closeModal($modal);
+    });
+  }
+
+  // Add a click event on buttons to open a specific modal
+  (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+    const modal = $trigger.dataset.target;
+    const $target = document.getElementById(modal);
+
+    $trigger.addEventListener('click', () => {
+      openModal($target);
+    });
+  });
+
+  // Add a click event on various child elements to close the parent modal
+  (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+    const $target = $close.closest('.modal');
+
+    $close.addEventListener('click', () => {
+      closeModal($target);
+    });
+  });
+
+  // Add a keyboard event to close all modals
+  document.addEventListener('keydown', (event) => {
+    if(event.key === "Escape") {
+      closeAllModals();
+    }
+  });
+});
+
+document.getElementById("contenido_modal").innerHTML = `
+    <p class="is-size-2">${mordekaiser.nombre}</p>
+    <p>${mordekaiser.descripcion}</p>
+    <span class="tag">${mordekaiser.tipo}</span>
+`;
+
+const div_jax = document.getElementById("jax");
+
+const mostrar_datos_jax = () => {
+    div_jax.innerHTML = `
+        <p class="is-size-2">${jax.nombre}</p>
+        <p>${jax.descripcion}</p>
+        <span class="tag">${jax.tipo}</span>
+    `;
+}
+
+const mostrar_imagen_jax = () => {
+    div_jax.innerHTML = `
+        <figure class="image">
+            <img class="is-rounded" src="${jax.imagen}" />
+        </figure>
+    `;
+}
+
+mostrar_imagen_jax();
+div_jax.onmouseover = mostrar_datos_jax;
+div_jax.onmouseleave = mostrar_imagen_jax;
 
