910841d1e11cc958793be45be62e7fb44e4538f1
NicolasBravoMiguel <a01713660@tec.mx>
2026-03-02 12:36:51 -0600

    crear modelo de personajes

--- LOL/routes/personaje.routes.js
+++ LOL/routes/personaje.routes.js
@@ -6,6 +6,9 @@ const personajesController = require('../controllers/personajes.controller');
 router.get('/new', personajesController.get_add);
 router.post('/new', personajesController.post_add);
 router.get('/old', personajesController.get_old);
+router.get('/nuevo', personajesController.get_add);
+router.post('/new', personajesController.post_add);
+router.get('/old', personajesController.get_old);
 router.use(personajesController.get_list);
 
 module.exports = router;
