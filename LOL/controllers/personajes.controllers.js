de29d439a3cb739bc2d6ed5cbc341bacae1270bd
NicolasBravoMiguel <a01713660@tec.mx>
2026-03-11 13:37:06 -0600

    correcrion de list.ejs y agregar html dinamico para la agregacion de personajes

--- LOL/controllers/personajes.controllers.js
+++ LOL/controllers/personajes.controllers.js
@@ -26,10 +26,11 @@ exports.get_old = (request, response, next) => {
 };
 
 exports.get_list = (request, response, next) => {
-    console.log(request.session.username);
+    console.log(request.session.permisos);
 
     Personaje.fetch(request.params.personaje_id).then(([rows, fieldData]) => {
         return response.render('list', {
+            permisos: request.session.permisos || [],
             username: request.session.username || '',
             personajes: rows,
         }); 
